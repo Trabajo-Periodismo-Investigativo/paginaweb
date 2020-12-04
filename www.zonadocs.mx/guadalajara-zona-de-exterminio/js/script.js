90629f23820b8391f6e77f23de1161c9ceea51bc
aedeanllano <30808302+aedeanllano@users.noreply.github.com>
2020-12-03 23:52:33 -0500

    Update script.js

--- www.zonadocs.mx/guadalajara-zona-de-exterminio/js/script.js
+++ www.zonadocs.mx/guadalajara-zona-de-exterminio/js/script.js
@@ -11,19 +11,21 @@ $(function() {
 
   var controller = new ScrollMagic.Controller();
 
-  var count1 = new CountUp('nmr01', 2,4, {});
-  var count2 = new CountUp('nmr02', 2,7, {});
-  var count3 = new CountUp('nmr03', 767137, {});
-  var count4 = new CountUp('nmr01', 25, {});
-  var count5 = new CountUp('nmr02', 232, {});
-  var count6 = new CountUp('nmr03', 319, {});
-  var count7 = new CountUp('nmr01', 30.000, {});
-  var count8 = new CountUp('nmr02', 78, {});
-  var count9 = new CountUp('nmr03', 321, {});
-  var count10 = new CountUp('nmr01', 12, {});
-  var count11 = new CountUp('nmr02', 17, {});
-  var count12 = new CountUp('nmr03', 24, {});
-   var count13 = new CountUp('nmr03', 56, {});
+  var count1 = new CountUp('nmr01', 2, {});
+  var count2 = new CountUp('nmr01', 4, {});
+  var count3 = new CountUp('nmr02', 2, {});
+  var count4 = new CountUp('nmr01', 7, {});
+  var count5 = new CountUp('nmr03', 767137, {});
+  var count6 = new CountUp('nmr01', 25, {});
+  var count7 = new CountUp('nmr02', 232, {});
+  var count8 = new CountUp('nmr03', 319, {});
+  var count9 = new CountUp('nmr01', 30.000, {});
+  var count10 = new CountUp('nmr02', 78, {});
+  var count11 = new CountUp('nmr03', 321, {});
+  var count12 = new CountUp('nmr01', 12, {});
+  var count13 = new CountUp('nmr02', 17, {});
+  var count14 = new CountUp('nmr03', 24, {});
+   var count15 = new CountUp('nmr03', 56, {});
 
   // Full screen galleries
   $(".fs-gallery").each(function(is, ns) {
